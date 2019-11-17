333fa2496af2c134f74db97f42d7cf74acb95f86
IAM-96 <56156953+IAM-96@users.noreply.github.com>
2019-11-18 00:14:30 +0300

    Adding camera functionality

--- app/src/main/java/com/example/expensetracker/list_epx_scr.java
+++ app/src/main/java/com/example/expensetracker/list_epx_scr.java
@@ -1,14 +1,59 @@
 package com.example.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class list_epx_scr extends AppCompatActivity {
+import java.io.File;
+
+public class list_epx_scr<Public> extends AppCompatActivity {
+
+    private android.graphics.Bitmap picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_epx_scr);
+
+
+            ImageButton ib = (ImageButton) findViewById(R.id.imageButton2);
+                    ib.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            takePhtoto();
+                        }
+
+                        });
+
+
     }
+   public void takePhtoto(){
+            Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera, 55);
+        }
+
+
+
+    protected void  onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 55) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ImageView imgv1 = findViewById(R.id.imgv1);
+            imgv1.setImageBitmap(photo);
+        }}
+
+
+
 }
