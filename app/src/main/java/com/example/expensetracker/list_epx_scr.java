d0bcb3dad4142b302a38d00dc13a10b58ad87c5f
AlwaleedAli <s1606167125@seu.edu.sa>
2019-11-22 05:21:52 +0300

    تم اضافة البطارية تبقى قاعدة بيانات

--- app/src/main/java/com/example/expensetracker/list_epx_scr.java
+++ app/src/main/java/com/example/expensetracker/list_epx_scr.java
@@ -5,16 +5,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -22,6 +26,14 @@ public class list_epx_scr<Public> extends AppCompatActivity {
 
     private android.graphics.Bitmap picture;
 
+    private TextView batteryTxt;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            batteryTxt.setText(batteryTxt.getText()+String.valueOf(level) + "%");
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +49,8 @@ public class list_epx_scr<Public> extends AppCompatActivity {
 
                         });
 
-
+         batteryTxt = (TextView) this.findViewById(R.id.batteryTxt);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
    public void takePhtoto(){
             Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
